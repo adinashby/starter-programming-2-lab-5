5742583a2ba41ec2bbf02694ceb3bdff78fe830a
Adin Ashby <adin.ashby@gmail.com>
2022-08-13 15:44:39 -0400

    first commit

---
+++ src/main/java/com/prog2/labs/LabFive.java
@@ -0,0 +1,59 @@
+package com.prog2.labs;
+
+/**
+ * @author adinashby
+ *
+ */
+
+public class LabFive {
+
+	/**
+	 * Write your test code below in the main (optional).
+	 *
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * 1) A magic square is an N-by-N matrix of the integers, such that 
+	 * all row, column, and diagonal sums are equal. For example,
+	 * 
+	 * 4	9	2
+	 * 3	5	7
+	 * 8	1	6
+	 * 
+	 * is a magic square, because the sum of row is 15 (4+9+2=15, 3+5+7=15, 8+1+6= 15), 
+	 * and the sum of each column is 15 (4+3+8=5, 9+5+1=15, 2+7+6=15) and the sum of 
+	 * the 2 diagonals is 15 (4+5+6=15, 2+5+8 = 15).
+	 * 
+	 * Write a Java method to test if a matrix represents a magic square.
+	 * 
+	 * 
+	 * @param matrix the two-dimensional input matrix to be tested.
+	 *
+	 * @return result true if it is a magic square.
+	 */
+
+	public boolean isMagicSquare(int[][] matrix) {
+		
+		return false;
+	}
+
+	/**
+	 * 2) Given an integer array nums of length n and an integer target, find three
+	 * integers in nums such that the sum is closest to target. Return the sum of
+	 * the three integers. You may assume that each input would have exactly one
+	 * solution.
+	 * 
+	 * @param nums   the input array.
+	 * @param target the target number.
+	 *
+	 * @return result the closest number to sum of 3.
+	 */
+
+	public int[][] magicSquareGenerator(int arraySize) {
+		
+		return new int[0][0];
+	}
+}
